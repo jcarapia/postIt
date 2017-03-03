82ebf8afc182b48e02703ae5bd4585be1df41560
Jorge <jcarapia83@Gmail.com>
2017-03-03 12:39:17 -0800

    styling note component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-import Header from '../Components/header'
+import Header from '../Components/header';
+import Note from '../Components/note';
 
 export default class App extends Component {
   render() {
     return (
       <div>
       <Header />
+      <Note />
+      <Note />
       </div>
     );
   }
--- src/components/note.js
+++ src/components/note.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 export default class Note extends Component {
   render() {
     return (
-      <div className="header">
-      	<button id="add-note" className="btn btn-add"><span>&#43;</span>Add Note</button>
+      <div className="note">
+      	<h4>Sample Note</h4>
       </div>
     );
   }
